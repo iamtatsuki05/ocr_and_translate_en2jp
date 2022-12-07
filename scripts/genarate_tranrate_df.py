6790b1b1c73c4ed823236c727ca5f4121915bd08
iamtatsuki05 <tatsukio0522@gmail.com>
2022-12-07 11:59:32 +0900

    add script

---
+++ scripts/genarate_tranrate_df.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from ocr_and_translate_en2jp.ocr import ocr_image
+from ocr_and_translate_en2jp.translate import generate_jp_word_type, translate_word
+
+
+def df_generator(
+    file_path,
+    output_path="./output.csv",
+    max_words=None,
+    is_return_shuffle=False,
+    seed=None,
+):
+    ocr_result = ocr_image(file_path, is_return_list=True)
+    ocr_result = list(set(ocr_result))
+    df = pd.DataFrame(ocr_result, columns=["word"])
+    df["word_pos"] = df["word"].apply(generate_jp_word_type)
+    df["word_JP"] = df["word"].apply(translate_word)
+
+    if max_words is None:
+        max_words = len(df)
+    if is_return_shuffle:
+        df = df.sample(n=max_words, random_state=seed, ignore_index=True)
+    else:
+        df = df.head(max_words)
+    df.to_csv(output_path, index=False)
+
+
+def main():
+    df_generator("")
+
+
+if __name__ == "__main__":
+    main()
